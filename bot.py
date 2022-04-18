b87c57b40c7bbc3bc82a0a5d0184e5933cf443f6
Dmitrii Kurashov <kda.f.daily@gmail.com>
2022-04-18 21:04:39 +0500

    update

--- bot.py
+++ bot.py
@@ -34,40 +34,61 @@ def start_keyboard():
 def campus_keyboard():
     
     buttons = [
-        types.InlineKeyboardButton(text = "1", callback_data="ulk_1")
+        types.InlineKeyboardButton(text="УЛК-1", callback_data="ulk_1"),
+        types.InlineKeyboardButton(text="Назад", callback_data="start_menu")
     ]
     
-    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     keyboard.add(*buttons)
 
     return keyboard
 
 
+def go_back_keyboard(point):
+    
+    button = types.InlineKeyboardButton(text="Назад", callback_data=point)
+
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(button)
+    
+    return keyboard
+
 @dp.message_handler(commands="help")
 async def send_welcome(message: types.Message):
 
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.", reply_markup=types.ReplyKeyboardRemove())
-
+    await message.reply("Бот-помошник для студентов")
 
 @dp.message_handler(commands="start")
 async def start_command(message: types.Message):
 
-    keyboard = start_keyboard()
+    await message.answer("Какую информацию вы хотите получить?", reply_markup=start_keyboard())
 
-    await message.answer("Какую информацию вы хотите получить?", reply_markup=keyboard)
+
+@dp.callback_query_handler(text="start_menu")
+async def start_menu(call: types.CallbackQuery):
+
+    await call.message.edit_text("Какую информацию вы хотите получить?", reply_markup=start_keyboard())
+    await call.answer()
 
 
 @dp.callback_query_handler(text="campus")
 async def send_campus_buttons(call: types.CallbackQuery):
 
-    await call.message.answer("Выберите корпус", reply_markup=campus_keyboard())
+    await call.message.edit_text("Выберите корпус", reply_markup=campus_keyboard())
     await call.answer()
 
 
 @dp.callback_query_handler(text="contacts")
 async def send_contacts(call: types.CallbackQuery):
     
-    await call.message.answer("Контакты")
+    await call.message.edit_text("Контакты", reply_markup=go_back_keyboard("start_menu"))
+    await call.answer()
+
+
+@dp.callback_query_handler(text="ulk_1")
+async def send_contacts(call: types.CallbackQuery):
+    
+    await call.message.edit_text("Улк-1", reply_markup=go_back_keyboard("campus"))
     await call.answer()
 
 
